aee671fab507381bec472801080a5ac7fa903670
Stuart Elimu <41069456+ElimuStuart@users.noreply.github.com>
2019-07-25 05:50:18 +0300

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+function App() {
+  return (
+    <div>
+      <input type="checkbox" /> <label>Go to the store.</label> <br />
+      <input type="checkbox" /> <label>Make bread.</label> <br />
+      <input type="checkbox" /> <label>Completed reading "Learn Django".</label>{" "}
+      <br />
+      <input type="checkbox" /> <label>Go jogging.</label>
+    </div>
+  );
+}
+
+export default App;
