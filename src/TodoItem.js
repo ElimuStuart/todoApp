ca3139f7525f367c2a614a3cd0d0aa1122d2240a
Stuart Elimu <41069456+ElimuStuart@users.noreply.github.com>
2019-08-17 21:38:35 +0300

    Phase 7
    Added completed style

--- src/TodoItem.js
+++ src/TodoItem.js
@@ -6,9 +6,15 @@ function TodoItem(props) {
     fontWeight: 'bold'
   }
 
+  const completedStyle = {
+    fontSyle: "italic",
+    color: "#cdcdcd",
+    textDecoration: "line-through"
+  }
+
   return (
     <div style={style}>
-      <input type="checkbox" onChange={() => props.handleChange(props.item.id)} checked={props.item.completed}/> <label>{props.item.text}</label> <br />
+      <input type="checkbox" onChange={() => props.handleChange(props.item.id)} checked={props.item.completed}/> <label style={props.item.completed ? completedStyle : null}>{props.item.text}</label> <br />
     </div>
   )
 
